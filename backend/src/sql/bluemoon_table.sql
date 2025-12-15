6196750b37bd5454a8ce1fe048febd014a2f9563
DuongAn15 <an.td235256@sis.hust.edu.vn>
2025-12-15 16:54:05 +0700

    feat: add fullname field to users table

--- backend/src/sql/bluemoon_table.sql
+++ backend/src/sql/bluemoon_table.sql
@@ -63,6 +63,7 @@ CREATE TABLE resident (
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    fullname VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password TEXT NOT NULL,
     status status NOT NULL DEFAULT 'inactive',
