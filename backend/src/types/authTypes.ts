01c16f3269112c5e8278a934927c95bddb9e2f4b
nguien <nguyen10t2lhp@gmail.com>
2025-12-17 21:34:27 +0700

    feat: update OTP service to use 'code' parameter and add auth types for login, refresh token, and OTP

---
+++ backend/src/types/authTypes.ts
@@ -0,0 +1,19 @@
+import { t } from "elysia";
+
+export const LoginBody = t.Object({
+    email: t.String({ format: "email"}),
+    password: t.String(),
+    name: t.String(),
+});
+
+export const RefreshTokenBody = t.Object({
+    user_id: t.String({ format: "uuid" }),
+    token: t.String(),
+    expires_at: t.String({ format: "date-time" }),
+});
+
+export const OtpBody = t.Object({
+    email: t.String({ format: "email" }),
+    code: t.String(),
+    expires_at: t.String({ format: "date-time" }),
+});
