f25dba118ac6a1d5ef707e7de0e9f4e2e597a79e
nguien <nguyen10t2lhp@gmail.com>
2025-12-28 16:04:07 +0700

    Refactor date fields in feeType and resident schemas to include mode, enhance update functions in house and resident services, and define update body types for better type safety

--- backend/src/types/residentTypes.ts
+++ backend/src/types/residentTypes.ts
@@ -1,4 +1,4 @@
-import { t } from "elysia";
+import { Static, t } from "elysia";
 import { enumToTypeBox } from "../helpers/enumHelper";
 import { gender, resident_status } from "../models/pgEnum";
 
@@ -18,10 +18,12 @@ export const UpdateResidentBody = t.Object({
     full_name: t.Optional(t.String()),
     id_card: t.Optional(t.String()),
     date_of_birth: t.Optional(t.Date()),
-    phone: t.String(),
+    phone: t.Optional(t.String()),
     gender: t.Optional(enumToTypeBox(gender.enumValues)),
     role: t.Optional(t.Number()),
     status: t.Optional(enumToTypeBox(resident_status.enumValues)),
     house_id: t.Optional(t.String({ format: "uuid" })),
     occupation: t.Optional(t.String()),
 });
+
+export type UpdateResidentBodyType = Static<typeof UpdateResidentBody>;
