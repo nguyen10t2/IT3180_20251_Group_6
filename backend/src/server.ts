b9abe8a2d6511843aacf6b0bbbf91ad861f3119a
HieuNH <gaconvuitinh2005@gmail.com>
2025-12-29 14:58:53 +0700

    feat: add get invoice and invoice details API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,7 @@ import { userRoutes } from "./handlers/userHandlers";
 import { notificationRoutes } from "./handlers/notificationHandler";
 import { residentRoutes } from "./handlers/residentHandlers";
 import { feedbackRoutes } from "./handlers/feedbackHandlers";
+import { invoiceRoutes } from "./handlers/invoiceHandlers";
 
 const hostname: string = Bun.env.IP_ADDRESS || '127.0.0.1';
 const port: number = Number(Bun.env.PORT || '3000');
@@ -29,6 +30,7 @@ new Elysia()
   .use(authorizationPlugins("resident"))
   .use(userRoutes)
   .use(residentRoutes)
+  .use(invoiceRoutes)
   .use(feedbackRoutes)
   .use(notificationRoutes)
   .listen({ hostname, port });
