a14da75d4cd81245f9092f967281eb11ee7eed7a
HieuNH <gaconvuitinh2005@gmail.com>
2025-12-31 11:50:43 +0700

    add resident API and invoice API for manager handler

--- backend/src/handlers/managerHandlers.ts
+++ backend/src/handlers/managerHandlers.ts
@@ -6,6 +6,10 @@ import { approveUser, getPendingUsers, getUserById, getUsersByLastCreatedAndLimi
 import { authorizationPlugins } from "../plugins/authorizationPlugins";
 import { householdRoutes } from "./householdHandlers";
 import { userRoutes } from "./userHandlers";
+import { deleteResident, getAll as getAllResident, getResidentById, updateResident } from "../services/residentServices";
+import { UpdateResidentBody } from "../types/residentTypes";
+import { confirmInvoice, createInvoice, deleteInvoice, getAll as getAllInvoice, getInvoiceById, updateInvoice } from "../services/invoiceServices";
+import { CreateInvoiceBody, UpdateInvoiceBody } from "../types/invoiceTypes";
 
 
 export const managerRoutes = new Elysia({ prefix: '/manager', detail: { tags: ['Manager'] } })
@@ -94,3 +98,160 @@ export const managerRoutes = new Elysia({ prefix: '/manager', detail: { tags: ['
       })
   )
   .use(householdRoutes)
+  .group('/resident', (app) =>
+    app
+      .get('/', async ({ status }) => {
+        try {
+          const res = await getAllResident();
+          return status(200, { residents: res?.data ?? [] });
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      })
+      .get('/:resident_id', async ({ params, status }) => {
+        try {
+          const res = await getResidentById(params.resident_id);
+          if (!res.data)
+            return status(404, { message: 'Không tìm thấy cư dân' });
+          return status(200, { resident: res.data });
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      })
+      .put('/:resident_id/update', async ({ body, params, status }) => {
+        if (body == null || typeof body !== "object" || Object.keys(body).length === 0) {
+          return status(400, { message: "Không có thông tin để cập nhật cư dân" });
+        }
+        try {
+          const fetchResident = await getResidentById(params.resident_id);
+          if (!fetchResident.data)
+            return status(404, { message: 'Không tìm thấy cư dân' });
+
+          const res = await updateResident(params.resident_id, body);
+          if (res.data)
+            return status(200, { message: 'Cập nhật thông tin cư dân thành công' });
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      }, {
+        body: UpdateResidentBody
+      })
+      .delete('/:resident_id/delete', async ({ params, status }) => {
+        try {
+          const fetchResident = await getResidentById(params.resident_id);
+          if (!fetchResident.data)
+            return status(404, { message: 'Không tìm thấy cư dân' });
+          const res = deleteResident(params.resident_id);
+          return status(200, { message: 'Xóa cư dân thành công' });
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      })
+  )
+  .group('/invoice', (app) =>
+    app
+      .get('/', async ({ status }) => {
+        try {
+          const res = await getAllInvoice();
+          return status(200, { invoices: res?.data ?? [] });
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      })
+      .get('/:invoice_id', async ({ params, status }) => {
+        try {
+          const res = await getInvoiceById(params.invoice_id);
+          if (!res.data)
+            return status(404, { message: 'Không tìm thấy hóa đơn' });
+          return status(200, { invoice: res.data });
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      })
+      .post('/', async ({ body, user, status }) => {
+        try {
+          const userId = user.id!;
+          const formattedBody = {
+            ...body,
+            created_by: userId
+          }
+          await createInvoice(formattedBody);
+          return status(200, { message: 'Tạo hóa đơn thành công' });
+
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      }, {
+        body: CreateInvoiceBody
+      })
+      .put('/:invoice_id/update', async ({ params, body, status }) => {
+        try {
+          const fetchInvoice = await getInvoiceById(params.invoice_id);
+          if (!fetchInvoice.data)
+            return status(404, { message: 'Không tìm thấy hóa đơn' });
+
+          const res = await updateInvoice(params.invoice_id, body);
+          if (res.data)
+            return status(200, { message: 'Cập nhật hóa đơn thành công' });
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      }, {
+        body: UpdateInvoiceBody
+      })
+      .delete('/:invoice_id/delete', async ({ params, status }) => {
+        try {
+          const fetchInvoice = await getInvoiceById(params.invoice_id);
+          if (!fetchInvoice.data)
+            return status(404, { message: 'Không tìm thấy hóa đơn' });
+
+          const res = await deleteInvoice(params.invoice_id);
+          if (res.data)
+            return status(200, { message: 'Xóa hóa đơn thành công' });
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      })
+      .post('/:invoice_id/confirm', async ({ params, body, user, status }) => {
+        try {
+          const fetchInvoice = await getInvoiceById(params.invoice_id);
+          if (!fetchInvoice.data)
+            return status(404, { message: 'Không tìm thấy hóa đơn' });
+
+          const confirmerId = user.id!;
+          const paidAmount = body?.paidAmount ?? "";
+          const paymentNote = body?.paymentNote ?? "";
+          const res = await confirmInvoice(params.invoice_id, confirmerId, paidAmount, paymentNote);
+
+          if(res.data)
+            return status(200, {message: 'Xác nhận thanh toán hóa đơn thành công'});
+        }
+        catch (error) {
+          console.error(error);
+          throw new HttpError(500, INTERNAL_SERVER_ERROR);
+        }
+      },{
+        body: t.Object({
+           paidAmount: t.Optional(t.String()),
+           paymentNote: t.Optional(t.String())
+        })
+      })
+  )
--- backend/src/types/invoiceTypes.ts
+++ backend/src/types/invoiceTypes.ts
@@ -2,22 +2,22 @@ import { t } from "elysia";
 import { enumToTypeBox } from "../helpers/enumHelper";
 import { fee_status } from "../models/pgEnum";
 
-export const CreateInvoice = t.Object({
-    house_id: t.String({ format: "uuid" }),
-    period_month: t.Number(),
-    period_year: t.Number(),
-    total_amount: t.Numeric({ precision: 12, scale: 2 }),
-    due_date: t.Date(),
-    invoice_type: t.Number(),
+export const CreateInvoiceBody = t.Object({
+    house_id: t.String({ format: "uuid", error: 'Không được để trống ID hộ dân' }),
+    period_month: t.Number({error: 'Không được để trống tháng của kỳ thanh toán'}),
+    period_year: t.Number({error: 'Không được để trống năm của kỳ thanh toán'}),
+    total_amount: t.String({error: 'Không được để trống số tiền của hóa đơn' }),
+    due_date: t.Date({error: 'Không được để trống hạn thanh toán hóa đơn'}),
+    invoice_type: t.Number({error: 'Không được để trống loại hóa đơn'}),
     notes: t.Optional(t.String()),
-    created_by: t.String({ format: "uuid" }),
+    created_by: t.String({ format: "uuid", error: 'Không được để trống người tạo hóa đơn' }),
 });
 
-export const UpdateInvoice = t.Object({
+export const UpdateInvoiceBody = t.Object({
     house_id: t.Optional(t.String({ format: "uuid" })),
     period_month: t.Optional(t.Number()),
     period_year: t.Optional(t.Number()),
-    total_amount: t.Optional(t.Numeric({ precision: 12, scale: 2 })),
+    total_amount: t.Optional(t.String()),
     due_date: t.Optional(t.Date()),
     invoice_type: t.Optional(t.Number()),
     notes: t.Optional(t.String()),
